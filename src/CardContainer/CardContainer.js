25e79fa23b38d001ded8824e05332afa67928e0e
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-06 16:36:44 -0600

    Proptypes for CardContainer

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
+import PropTypes from 'prop-types';
 
 
 export const CardContainer = ({ peopleInfo, planetInfo, vehicleInfo }) => {
@@ -10,3 +11,8 @@ export const CardContainer = ({ peopleInfo, planetInfo, vehicleInfo }) => {
 
 };
 
+CardContainer.propTypes = {
+  peopleInfo: PropTypes.array,
+  planetInfo: PropTypes.array,
+  vehicleInfo: PropTypes.array
+};
