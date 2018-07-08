ac2ab938c490cc327f6da08600e7f263860237a5
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-07 20:02:54 -0600

    Stub out tests for Cleaner.js

---
+++ src/Cleaner/Cleaner.test.js
@@ -0,0 +1,19 @@
+import cleaner from './Cleaner';
+
+describe('cleaner', () => {
+  it('should return the correct data when passed a argument of films', () => {
+
+  });
+
+  it('should return the correct data when passed the argument people', () => {
+
+  });
+
+  it('should return the correct data when passed the argument of planets', () => {
+
+  });
+
+  it('should return the correct data when passed the argument of vehicles', () => {
+
+  });
+});
